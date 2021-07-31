e658fa0075603a45f722e1e4dc10900faf5e4feb
Douglas-Castro <farias.douglascastro@gmail.com>
2021-07-31 01:45:40 -0300

    :bulb: Add base

---
+++ game.js
@@ -0,0 +1,7 @@
+console.log('Douglas Castro - Flappy Bird');
+
+const sprites = new Image();
+sprites.src = './sprites.png';
+
+const canvas = document.querySelector('canvas');
+const contexto = canvas.getContext('2d');
